941531f39b466da0da5057070ae29bb9d6d9671c
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2025-06-26 20:50:28 +1000

    feat: Only recurse over .X file format
    * feat: Use custom logger pattern
    
    * fix: Use string default value instead of c_str
    
    * feat: Use ms for stopwatch

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,9 @@
 #include "spdlog/stopwatch.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
-int main(const int argc, const char **argv) {
+int main(const int argc, const char** argv) {
     set_default_logger(spdlog::stdout_color_mt(PROJECT_NAME));
+    spdlog::set_pattern(LOGGER_PATTERN);
     spdlog::enable_backtrace(5);
     spdlog::set_level(spdlog::level::info);
 
@@ -23,22 +24,22 @@ int main(const int argc, const char **argv) {
     options.add_options()
         ("v,verbose", "Enable verbose log output")
         ("h,help", "Print program usage");
-    options.add_options("assets")
+    options.add_options("asset")
         ("i,input", "Path to input directory", cxxopts::value<std::string>())
         (
-            "o,output",
-            "(optional) Path to output directory",
-            cxxopts::value<std::string>()->default_value((exe_dir / "out").c_str())
+            "o,output", "(optional) Path to output directory",
+            cxxopts::value<std::string>()->default_value((exe_dir / "out").string())
         )
         ("f,fix-assets", "Fix input asset files")
         ("a,convert-assets", "Convert assets");
-    options.add_options("maps")
+    options.add_options("map")
         ("m,convert-maps", "Convert maps");
     options.parse_positional({ "input" });
     const auto result = options.parse(argc, argv);
 
     if (result["verbose"].as<bool>()) {
         spdlog::set_level(spdlog::level::trace);
+        spdlog::disable_backtrace();
         Assimp::DefaultLogger::create("assimp.log", Assimp::Logger::VERBOSE);
     }
     if (result["help"].as<bool>()) {
@@ -47,41 +48,40 @@ int main(const int argc, const char **argv) {
     }
 
     if (result.count("input") == 0) {
-        SPDLOG_WARN("No input directory specified!  Please pass in the directory containing your .x files.");
         SPDLOG_INFO(options.help());
         return 1;
     }
 
-    if (!result["fix-assets"].as<bool>() && !result["convert-assets"].as<bool>()) {
-        SPDLOG_WARN("No action requested!  Please include -a and/or -f");
-        SPDLOG_INFO(options.help());
-        return 1;
+    const auto in_dir = fs::path(result["input"].as<std::string>());
+    const auto out_dir = fs::path(result["output"].as<std::string>());
+
+    SPDLOG_INFO("Looking for assets in {}", in_dir.string());
+
+    const auto out_fixed_dir = out_dir / "fixed";
+    if (fs::exists(out_fixed_dir) && !out_fixed_dir.empty()) {
+        SPDLOG_WARN("Found non-empty fixed directory. Potential for recursive conversion.");
     }
 
-    // yummyyy
-    const fs::path in_dir = std::filesystem::u8path(result["input"].as<std::string>());
-    const fs::path out_dir = std::filesystem::u8path(result["output"].as<std::string>());
-
-
-    SPDLOG_INFO("Looking for assets in {}", in_dir.c_str());
-
-    const fs::path out_fixed_dir = out_dir / "fixed";
-    const fs::path out_assets_dir = out_dir / "assets";
+    const auto out_assets_dir = out_dir / "assets";
+    if (fs::exists(out_assets_dir) && !out_assets_dir.empty()) {
+        SPDLOG_WARN("Found non-empty fixed directory. Potential for recursive conversion.");
+    }
 
     // Fix the assets if needed.
     if (result["fix-assets"].as<bool>()) {
         spdlog::stopwatch sw;
-        for (const auto &entry: fs::recursive_directory_iterator(in_dir)) {
+        for (const auto& entry: fs::recursive_directory_iterator(in_dir)) {
             if (!entry.is_regular_file()) continue;
-            const auto &entry_path = entry.path();
-            const auto out = out_fixed_dir / relative(entry_path, in_dir);
+            const auto& entry_path = entry.path();
+            const auto out = out_fixed_dir / fs::relative(entry_path, in_dir);
 
             if (!asset::fix(entry_path, out)) {
                 SPDLOG_ERROR("Failed to fix all assets. Stopped to prevent issues.");
+                spdlog::dump_backtrace();
                 return 1;
             }
         }
-        SPDLOG_INFO("Fixing assets took {:.2}s", sw);
+        SPDLOG_INFO("Fixing assets took {}ms", sw.elapsed_ms().count());
     }
 
     // Convert the assets if needed.
@@ -89,19 +89,23 @@ int main(const int argc, const char **argv) {
         const auto in = result["fix-assets"].as<bool>() ? out_fixed_dir : in_dir;
 
         spdlog::stopwatch sw;
-        for (const auto &entry: fs::recursive_directory_iterator(in)) {
+        for (const auto& entry: fs::recursive_directory_iterator(in)) {
+            const auto& entry_path = entry.path();
+            const auto& entry_ext = entry_path.extension().string();
             if (!entry.is_regular_file()) continue;
-            const auto &entry_path = entry.path();
-            const auto out_path = (out_assets_dir / relative(entry_path, in)).replace_extension("gltf");
+            if (entry_ext != ".X" || entry_ext != ".x") continue;
+            const auto out_path = (out_assets_dir / fs::relative(entry_path, in))
+                .replace_extension("gltf");
 
             if (!asset::load(entry_path) || !asset::dump("gltf2", out_path)) {
                 SPDLOG_ERROR("Failed to dump all assets. Stopped to prevent issues.");
+                spdlog::dump_backtrace();
                 return 1;
             }
         }
-        SPDLOG_INFO("Converting assets took {:.2}s", sw);
+        SPDLOG_INFO("Converting assets took {}ms", sw.elapsed_ms().count());
     }
 
-    SPDLOG_INFO("💖 Completed! Have a nice day.");
+    SPDLOG_INFO("<3 Completed! Have a nice day.");
     return 0;
 }
